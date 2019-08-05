362a4e51669dabe3fec9b3a877e2c305e58b2c59
David De Marco <david.demarco@demarcogaming.com>
2019-08-04 19:39:12 -0700

    Initial upload with build exe (Windows/Android)
    :wq

---
+++ Assets/Scripts/Cricket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cricket : MonoBehaviour {
+
+	public float minimumPitch = 0.9f;
+	public float maximumPitch = 1.1f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().pitch = Random.Range (minimumPitch, maximumPitch);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int scoreToWin;
+    public int curScore;
+
+    public bool gamePaused;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        // set the instance to this script
+        instance = this;
+    }
+
+    void Start ()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    void Update ()
+    {
+        if(Input.GetButtonDown("Cancel"))
+            TogglePauseGame();
+    }
+
+    public void TogglePauseGame ()
+    {
+        gamePaused = !gamePaused;
+        Time.timeScale = gamePaused == true ? 0.0f : 1.0f;
+
+        Cursor.lockState = gamePaused == true ? CursorLockMode.None : CursorLockMode.Locked;
+
+        // toggle the pause menu
+        GameUI.instance.TogglePauseMenu(gamePaused);
+    }
+
+    public void AddScore (int score)
+    {
+        curScore += score;
+
+        // update the score text
+        GameUI.instance.UpdateScoreText(curScore);
+
+        // have we reached the score to win?
+        if(curScore >= scoreToWin)
+            LevelEnd();
+    }
+
+    // called when the player enters a goal
+    public void LevelEnd()
+    {
+        // is this the last level?
+        if (SceneManager.sceneCountInBuildSettings == SceneManager.GetActiveScene().buildIndex + 1)
+        {
+            WinGame();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void WinGame ()
+    {
+        // set the end game screen
+        GameUI.instance.SetEndGameScreen(true, curScore);
+
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoseGame ()
+    {
+        // set the end game screen
+        GameUI.instance.SetEndGameScreen(false, curScore);
+
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
